cbcd944c62441fc183260a73b978515542cdebf3
NOTFELIPE <NOTFELIPE@NOTFELIPE-PC>
2016-06-17 15:14:56 -0300

    Merge origin/exercicio1
    Conflicts:
    	README.md

---
+++ src/Televisor.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * @author Felipe Rodrigues Gil - RA 248543
+ * Variáveis declaradas para o funcionamento do televisor.
+ */
+public class Televisor {
+    public int canal;
+    public int volume;
+    public boolean estado;
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
+   
+}
